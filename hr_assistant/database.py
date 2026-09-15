1f0f3511bc9bd5aebd3930a02643bd6ccb0a9fdb
Adamo Jr <adamojr@outlook.it>
2026-09-15 16:05:45 +0200

    hr-assistant avanzamento 6, spero di aver capito e sistemato l'errore; ora la ricerca dei dati è tramite ChromaDB e pesca tutti i file disponibili

--- hr_assistant/database.py
+++ hr_assistant/database.py
@@ -1,45 +1,19 @@
-class MockCollection:
-    def __init__(self):
-        self.items = []
-
-    def get(self, ids=None, where=None, limit=None, offset=None, include=None):
-        metadatas = [item["metadata"] for item in self.items if "metadata" in item]
-        ids_list = [item["id"] for item in self.items if "id" in item]
-        documents = [item["document"] for item in self.items if "document" in item]
-        
-        res = {}
-        if include:
-            if "metadatas" in include:
-                res["metadatas"] = metadatas
-            if "documents" in include:
-                res["documents"] = documents
-            if "ids" in include:
-                res["ids"] = ids_list
-        else:
-            res = {"ids": ids_list, "metadatas": metadatas, "documents": documents}
-        return res
-
-    def add(self, documents, metadatas=None, ids=None):
-        for i, doc in enumerate(documents):
-            doc_id = ids[i] if ids and i < len(ids) else str(len(self.items))
-            meta = metadatas[i] if metadatas and i < len(metadatas) else {}
-            self.items.append({"id": doc_id, "document": doc, "metadata": meta})
-
-    def query(self, query_texts, n_results=2, include=None):
-        docs = [item["document"] for item in self.items]
-        metas = [item["metadata"] for item in self.items]
-        return {
-            "documents": [docs[:n_results]],
-            "metadatas": [metas[:n_results]],
-            "distances": [[0.1] * min(n_results, len(docs))]
-        }
+import chromadb
+from pathlib import Path
 
 class Database:
-    def __init__(self, collection_name="hr_resumes"):
-        self.collection_name = collection_name
-        self._collection = MockCollection()
+    def __init__(self, collection_name="hr_resumes", persist_directory: str = "./chroma_db"):
+        """Inizializza un database ChromaDB persistente su disco per il vero recupero semantico."""
+        self.persist_path = Path(persist_directory)
+        self.persist_path.mkdir(parents=True, exist_ok=True)
+        
+        self.client = chromadb.PersistentClient(path=str(self.persist_path))
+        
+        self._collection = self.client.get_or_create_collection(name=collection_name)
 
     def get_or_create_collection(self, name=None):
+        if name and name != self._collection.name:
+            return self.client.get_or_create_collection(name=name)
         return self._collection
 
     def get_collection(self):
